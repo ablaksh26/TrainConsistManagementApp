b2610fc82a479faf4a5511c7f33eb7a2a3b71edb
merock2024 <merock2024@gmail.com>
2026-03-10 14:13:44 +0530

    [Abhilaksh] Coded the UC2 that uses add and remove operations of ArrayList.

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -3,25 +3,40 @@ package com.main;
 import java.util.*;
 
 /*
- * 
  * @author: Abhilaksh
- * @version: 1.0
+ * @version: UC2
  * 
- * 
- * */
-
+ */
 
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("====================================");
+		System.out.println("====Train Consist Management App====");
+		System.out.println("====================================");
 		
 		List<String> trainConsist = new ArrayList<>();
 		
-		System.out.println("========================================");
-		System.out.println("======Train Consist Management App======");
-		System.out.println("========================================");
+		trainConsist.add("Sleeper");
+		trainConsist.add("AC Chair");
+		trainConsist.add("First Class");
 		
-		System.out.println("Initial Bogie Count: "+trainConsist.size());
+		System.out.println("After adding Bogies: ");
+		System.out.println("Passenger Bogies: " + trainConsist);
+		System.out.println();
+		
+		System.out.println("After removing AC Chair: ");
+		trainConsist.remove("AC Chair");
+		System.out.println("Passenger Bogies: " + trainConsist);
+		System.out.println();
+		
+		System.out.println("Checking if Sleeper exist?");
+		System.out.print("Contains Sleeper: ");
+		System.out.println(trainConsist.contains("Sleeper"));
+		System.out.println();
+		
+		System.out.println("Final Train Passenger Consist: " + trainConsist);
+
 	}
 
 }
