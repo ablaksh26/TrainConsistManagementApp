fa26b38e7db24c65505e1e2227b96d3debcc311b
merock2024 <merock2024@gmail.com>
2026-03-12 10:45:55 +0530

    [Abhilaksh] Coded the UC9 that group bogies by type.

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -9,7 +9,7 @@ import com.bogie.Bogie;
  * 
  * 
  * @author: Abhilaksh
- * @version: UC8
+ * @version: UC9
  * 
  * 
  */
@@ -17,33 +17,44 @@ import com.bogie.Bogie;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		System.out.println("====================================");
-		System.out.println("====Train Consist Management App====");
-		System.out.println("====================================");
-		System.out.println("");
+		System.out.println("==================================");
+		System.out.println("===Train Consist Management App===");
+		System.out.println("==================================");
+		System.out.println();
 		
 		List<Bogie> bogies = new ArrayList<>();
 		
+		// Adding The New Bogies class and capacity
 		bogies.add(new Bogie("Sleeper",72));
 		bogies.add(new Bogie("AC Chair",56));
 		bogies.add(new Bogie("First Class",24));
+		bogies.add(new Bogie("Sleeper",92));
+		bogies.add(new Bogie("AC Chair",66));
 		bogies.add(new Bogie("General",90));
 		
 		System.out.println("All Bogies:");
 		
+		// Printing all capacities
 		for(Bogie b : bogies) {
 			System.out.println(b.name + " -> " + b.capacity);
 		}
 		
-		bogies = bogies.stream().filter(bogie -> bogie.capacity > 60).collect(Collectors.toList());
+		Map<String,List<Bogie>> groupedMap = bogies.stream().collect(Collectors.groupingBy(bogie -> bogie.name));
 		
 		System.out.println();
-		System.out.println("Filtered Bogies > 60:");
-
-		for(Bogie b : bogies) {
-			System.out.println(b.name + " -> " + b.capacity);
+		System.out.println("Grouped Bogies:\n");
+		
+		// first getting all the same classes from grouped keys.
+		
+		for(String key : groupedMap.keySet()) {
+			System.out.println("Bogie Type: " + key);
+			for(Bogie b : groupedMap.get(key)) {
+				System.out.println("Capacity -> " + b.capacity);
+			}
+			System.out.println();
 		}
+		
+
 	}
 
 }
