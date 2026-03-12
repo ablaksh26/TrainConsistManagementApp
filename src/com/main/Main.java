e9d31d562ad62f6d705499cb5380537582d0fd2b
merock2024 <merock2024@gmail.com>
2026-03-12 16:11:11 +0530

    [Abhilaksh] Coded the UC14 that handles a custom exception.

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -4,48 +4,45 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import com.bogie.Bogie;
-
+import com.bogie.CargoBogie;
 
 /*
  *
  * 		 
  * @author: Abhilaksh
- * @version: UC13
+ * @version: UC14
  * 
  * 
  */
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		System.out.println("==================================");
 		System.out.println("===Train Consist Management App===");
 		System.out.println("==================================");
 		System.out.println();
 		
-		List<Bogie> bogies = new ArrayList<>();
 
-		bogies.add(new Bogie("Sleeper",72));
-		bogies.add(new Bogie("AC Chair",56));
-		bogies.add(new Bogie("First Class",24));
-		bogies.add(new Bogie("General",90));
+		try{
 
-		long startTime1 = System.nanoTime();
-		List<Bogie> list1 = bogies.stream().filter(bogie -> bogie.capacity > 60).collect(Collectors.toList());
-		long endTime1 = System.nanoTime();
-		
-		List<Bogie> list2 = new ArrayList<>();
-		long startTime2 = System.nanoTime();
-		for(Bogie bogie : bogies) {
-			if(bogie.capacity > 60) {
-				list2.add(bogie);
-			}
+			Bogie b1 = new Bogie("Sleeper",72);	
+			System.out.println("Created Bogie: " + b1.name + " -> " + b1.capacity);
+			
+			Bogie b2 = new Bogie("Sleeper",0);	
+			System.out.println("Created Bogie: " + b2.name + " -> " + b2.capacity);
+			
+		}catch(InvalidCapacityException e) {
+			System.out.println("Error: " + e.getMessage());
 		}
-		long endTime2 = System.nanoTime();
 		
 		
-		System.out.println("Stream Execution Time(ns): " + (endTime1-startTime1));
-		System.out.println("Loop Execution Time(ns): " + (endTime2-startTime2));
+	}
+	
+	public static class InvalidCapacityException extends Exception{
+		public InvalidCapacityException(String message){
+			super(message);
+		}
 	}
 
 }
