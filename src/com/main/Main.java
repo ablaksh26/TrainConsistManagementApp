12384f0b8d53fff6d517467923264b01904fe66f
merock2024 <merock2024@gmail.com>
2026-03-12 11:46:10 +0530

    [Abhilaksh] Coded the UC10 that prints the total capacity.

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,15 +1,13 @@
 package com.main;
 
 import java.util.*;
-import java.util.stream.Collectors;
+
 import com.bogie.Bogie;
 
-
 /*
- * 
- * 
+ * 		 
  * @author: Abhilaksh
- * @version: UC9
+ * @version: UC10
  * 
  * 
  */
@@ -22,9 +20,10 @@ public class Main {
 		System.out.println("==================================");
 		System.out.println();
 		
+		// Creating the Array List of bogies 
 		List<Bogie> bogies = new ArrayList<>();
 		
-		// Adding The New Bogies class and capacity
+		// Adding the bogies:
 		bogies.add(new Bogie("Sleeper",72));
 		bogies.add(new Bogie("AC Chair",56));
 		bogies.add(new Bogie("First Class",24));
@@ -34,26 +33,16 @@ public class Main {
 		
 		System.out.println("All Bogies:");
 		
-		// Printing all capacities
+		// Looping through the bogies and printing capacity:
 		for(Bogie b : bogies) {
 			System.out.println(b.name + " -> " + b.capacity);
 		}
 		
-		Map<String,List<Bogie>> groupedMap = bogies.stream().collect(Collectors.groupingBy(bogie -> bogie.name));
+		int totalCapacity = bogies.stream().map(bogie -> bogie.capacity).reduce(0, Integer::sum);
 		
 		System.out.println();
-		System.out.println("Grouped Bogies:\n");
-		
-		// first getting all the same classes from grouped keys.
-		
-		for(String key : groupedMap.keySet()) {
-			System.out.println("Bogie Type: " + key);
-			for(Bogie b : groupedMap.get(key)) {
-				System.out.println("Capacity -> " + b.capacity);
-			}
-			System.out.println();
-		}
-		
+		System.out.println("Total seating capacity of train: " + totalCapacity);
+
 
 	}
 
