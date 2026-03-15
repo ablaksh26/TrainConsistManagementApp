2d7d44d9e28f0a0c89d88a307c7bf47cde3ce908
merock2024 <merock2024@gmail.com>
2026-03-15 21:33:48 +0530

    [Abhilaksh] Coded the UC20 that performs exception handling.

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,53 +1,55 @@
 package com.main;
 
-
-
 /*
  *
- * 		 
+ * 
  * @author: Abhilaksh
- * @version: UC19
+ * @version: UC20
  * 
  * 
  * 
  */
 
-
 public class Main {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("==================================");
 		System.out.println("===Train Consist Management App===");
 		System.out.println("==================================");
 		System.out.println();
-
-		String[] bogies = {"BG101","BG102","BG103","BG104","BG105"};
 		
+		
+		String[] bogies = {};
+		
+		if(bogies.length == 0) {
+			throw new IllegalStateException("No bogies available in train consist. Search operation cannot proceed.");
+		}
 		System.out.println("Available Bogie IDs:");
-		for(String s : bogies) System.out.println(s);
 		
+		for(String s : bogies) System.out.println(s);
+
 		String searchID = "BG103";
 		boolean found = false;
-		
+
 		int left = 0;
-        int right = bogies.length - 1;
+		int right = bogies.length - 1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
 
-            int cmp = searchID.compareTo(bogies[mid]);
+			int cmp = searchID.compareTo(bogies[mid]);
 
-            if (cmp == 0) {
-                found = true;
-                break;
-            } else if (cmp > 0) {
-                left = mid + 1; 
-            } else {
-                right = mid - 1; 
-            }
-        }
+			if (cmp == 0) {
+				found = true;
+				break;
+			} else if (cmp > 0) {
+				left = mid + 1; 
+			} else {
+				right = mid - 1; 
+			}
+		}
 		System.out.println();
-		
+
 		if(found) System.out.println("Bogie " + searchID + " found in train consist");
 		else System.out.println("Bogie " + searchID + " not found in train consist");
 	}
